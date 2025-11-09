3b6a44ba48e0c754f3526b2637bc1a92de21e458
Mah0j0 <spm2028248@est.univalle.edu>
2025-11-09 09:12:17 -0400

    Pantalla de muerte

--- Assets/Scripts/Menu/Menu_behavior.cs
+++ Assets/Scripts/Menu/Menu_behavior.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class Menu_behavior : MonoBehaviour
 {
-//Funcionalidad para botones de menu
-//START
-public void StartGame()
+    //Funcionalidad para botones de menu
+    //START
+    public void StartGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Intro");
     }
-//ABOUT
-public void AboutGame()
-{
-    UnityEngine.SceneManagement.SceneManager.LoadScene("About");
-}
-//EXIT
-public void QuitGame()
-{
-    Application.Quit();
-    Debug.Log("Quit");
-}
+    //ABOUT
+    public void AboutGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("About");
+    }
+    //EXIT
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+    //MENU
+    public void BackToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+    //GAME OVER
+    public void BackToGameOver()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game_Over");
+    }
+    //NIVEL 2
+    public void BackToNivel2()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_2");
+    }
+    //NIVEL 1
+    public void BackToNivel1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel_1");
+    }
 
 }
